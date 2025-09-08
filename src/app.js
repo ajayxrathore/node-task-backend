a74c6a0bc1fe0a3ac617a047c96fe3a6bc097675
Ajay Rathore <ajayxrathore@gmail.com>
2025-09-08 20:33:41 +0530

    add url to handle req

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { createUser, getUsers } from "./controllers/user.controller.js";
 import cors from "cors";
 const app = express();
 app.use(cors(
-{ origin: "http://localhost:3000" }
+{ origin: "https://node-task.pages.dev/" }
 ));
 app.use(express.json());
 // app.get("/", (req, res) => {
